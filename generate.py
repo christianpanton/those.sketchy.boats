0f978375eed72f5a6d268dd9edfb57e013fbc04d
Christian Panton <christian@panton.org>
2025-06-30 00:35:59 +0200

    add headers

--- generate.py
+++ generate.py
@@ -1,11 +1,8 @@
 import json
-import os
 
 from sketchpy import builder
 
 build = builder.build()
-
-os.makedirs("dist", exist_ok=True)
 
 with open("dist/boats.json", "w") as gen:
     json.dump(build, gen)
