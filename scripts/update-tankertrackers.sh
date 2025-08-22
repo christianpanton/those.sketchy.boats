bfe28eb903e4a00da885bd6c791970debb109dc2
Christian Panton <christian@panton.org>
2025-08-22 17:58:11 +0200

    update description of tankertrackers.com

--- scripts/update-tankertrackers.sh
+++ scripts/update-tankertrackers.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo "# TankerTrackers blacklisted vessels" > ../src/sanctions/tankertrackers.imo
+echo "# [TankerTrackers.com](https://tankertrackers.com/report/sanctioned) Officially Blacklisted Tankers" > ../src/sanctions/tankertrackers.imo
 
-curl https://tankertrackers.com/api/sanctioned/v1 | jq -r '.data[] | [.imo, (.sanctions | keys | join(" ")) ] | @csv' | tr '"' ' ' | tr ',' ' ' | sort >> ../src/sanctions/tankertrackers.imo
+curl https://tankertrackers.com/api/sanctioned/v1 \
+    | jq -r '.data[] | [.imo, (.sanctions | keys | join(" ")) ] | @csv' \
+    | tr '"' ' ' | tr ',' ' ' \
+    | sort >> ../src/sanctions/tankertrackers.imo
